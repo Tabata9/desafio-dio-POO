55e5cb462363e1084f02c0ecee174e496ab6f455
Tabata9 <tabatarsouza@gmail.com>
2022-08-01 18:30:35 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,8 +23,40 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devTabata = new Dev();
+        devTabata.setNome("Tabata");
+        devTabata.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Tabata:" + devTabata.getConteudosInscritos());
+        devTabata.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Tabata: " + devTabata.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Tabata:" + devTabata.getConteudosConcluidos());
+        System.out.println("XP:" + devTabata.calcularTotalXp());
+
+        System.out.println("----------");
+
+
+        Dev devGiovanna = new Dev();
+        devGiovanna.setNome("Giovanna");
+        devGiovanna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Giovanna" + devGiovanna.getConteudosInscritos());
+        devGiovanna.progredir();
+        devGiovanna.progredir();
+        devGiovanna.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Giovanna: " + devGiovanna.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Giovanna: " + devGiovanna.getConteudosConcluidos());
+        System.out.println("XP:" + devTabata.calcularTotalXp());
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,37 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String descricao;
+
+    public abstract double calcularXp();
+
+    public static double getXpPadrao() {
+        return XP_PADRAO;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public abstract void setCargaHoraria(int i);
+
+    public abstract void setData(LocalDate now);
+}
